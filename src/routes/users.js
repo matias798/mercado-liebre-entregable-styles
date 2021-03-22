f19b77cacdf8bc691145db8c9f3bcb9d0ecffe08
mati <mati>
2021-03-22 14:08:15 -0300

    cree formulario de login

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'registro' });
+});
+
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
 module.exports = router;
